36f61984586ee4dc9f96504ca41513ba532b7d3b
Marc Lynch <lynchmarc2@gmail.com>
2021-12-23 14:03:02 -0600

    fix: autofix plugin

--- packages/cli/test/ts-migrate/ts-migrate-autofix.test.ts
+++ packages/cli/test/ts-migrate/ts-migrate-autofix.test.ts
@@ -1,9 +1,19 @@
 import { resolve } from "path";
 import { Reporter } from "@rehearsal/reporter";
+import { sleep } from "../../src/utils";
+
 import { tsMigrateAutofix } from "../../src/index";
 
 const FIXTURE_APP_PATH = resolve(__dirname, "../fixtures/app");
 const reporter = new Reporter({ cwd: process.cwd() });
 reporter.setCWD(FIXTURE_APP_PATH);
 
-tsMigrateAutofix(FIXTURE_APP_PATH, reporter);
+async function run() {
+  const exitCode = await tsMigrateAutofix(FIXTURE_APP_PATH, reporter);
+  await sleep(2000);
+  await reporter.end();
+
+  console.log(exitCode);
+}
+
+run();
