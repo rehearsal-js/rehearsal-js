1552b8f7eee52b7904546dc93c642e737b5e40d3
egorio <justegorio@gmail.com>
2022-02-23 09:40:56 -0800

    feat: Improve autofix plugin

---
+++ packages/plugin-ts-migrate/test/utils.ts
@@ -0,0 +1,31 @@
+import { PluginParams } from 'ts-migrate-server';
+import { createProject } from '@ts-morph/bootstrap';
+
+export async function realPluginParams<TOptions = unknown>(params: {
+  fileName?: string;
+  text?: string;
+  options?: TOptions;
+}): Promise<PluginParams<TOptions>> {
+  const { fileName = 'file.ts', text = '', options = {} } = params;
+
+  const project = await createProject({
+    compilerOptions: {
+      strict: true,
+      noUnusedLocals: true,
+      noUnusedParameters: true,
+    },
+    useInMemoryFileSystem: true,
+  });
+  const sourceFile = project.createSourceFile(fileName, text);
+
+  const getLanguageService = () => project.getLanguageService();
+
+  return {
+    options: options as unknown as TOptions,
+    fileName,
+    rootDir: __dirname,
+    text,
+    sourceFile,
+    getLanguageService,
+  };
+}
