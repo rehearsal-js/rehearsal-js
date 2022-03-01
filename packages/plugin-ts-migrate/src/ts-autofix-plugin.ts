2516d014b53a2c6a6a2459315f8fcbbc4ad0333d
Marc Lynch <lynchmarc2@gmail.com>
2022-03-01 08:39:16 -0600

    chore: tighten types

--- packages/plugin-ts-migrate/src/ts-autofix-plugin.ts
+++ packages/plugin-ts-migrate/src/ts-autofix-plugin.ts
@@ -2,14 +2,12 @@ import { Plugin } from "ts-migrate-server";
 
 import type { Reporter } from "@rehearsal/reporter";
 
-type Options = { reporter: Reporter };
-
 /**
  * This ts-migrate plugin will match against "ts-migrate" comments per file
  * parse the typescript diagnostic code into a more helpful comment
  * and if possible will autofix and mitigate based on the diagnostic code
  */
-const tsAutofixPlugin: Plugin<Options> = {
+const tsAutofixPlugin: Plugin<{ reporter: Reporter }> = {
   name: "ts-autofix-plugin",
   async run({ fileName, text, getLanguageService }) {
     const diagnostics = getLanguageService().getSemanticDiagnostics(fileName);
