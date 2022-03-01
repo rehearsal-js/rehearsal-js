6b786eefd9f6eab5b914a9b99538e84a17f3d14e
Marc Lynch <lynchmarc2@gmail.com>
2022-03-01 09:29:18 -0600

    fix(plugin-ts-migrate): inc updateCommentsOnly in plugin options interface

--- packages/plugin-ts-migrate/src/ts-autofix-plugin.ts
+++ packages/plugin-ts-migrate/src/ts-autofix-plugin.ts
@@ -7,7 +7,10 @@ import type { Reporter } from "@rehearsal/reporter";
  * parse the typescript diagnostic code into a more helpful comment
  * and if possible will autofix and mitigate based on the diagnostic code
  */
-const tsAutofixPlugin: Plugin<{ reporter: Reporter }> = {
+const tsAutofixPlugin: Plugin<{
+  reporter: Reporter;
+  updateCommentsOnly?: boolean;
+}> = {
   name: "ts-autofix-plugin",
   async run({ fileName, text, getLanguageService }) {
     const diagnostics = getLanguageService().getSemanticDiagnostics(fileName);
