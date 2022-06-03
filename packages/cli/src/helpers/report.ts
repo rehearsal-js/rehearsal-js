4ee9366d6236bd12a24474fad3c3b711d318e70c
egorio <justegorio@gmail.com>
2022-06-03 16:36:52 -0700

    refactor: Add "count per error" to the report

--- packages/cli/src/helpers/report.ts
+++ packages/cli/src/helpers/report.ts
@@ -7,21 +7,31 @@ export function reportFormatter(report: Report): string {
   const fileNames = [
     ...new Set(report.items.map((item) => item.file.replace(report.summary.basePath, ''))),
   ];
-  const uniqueErrors = [...new Set(report.items.map((item) => item.code))];
-  const autofixedErrors = [
-    ...new Set(report.items.map((item) => (item.fixed ? item.code : undefined)).filter(Boolean)),
-  ];
+
+  const fixedErrors: { [key: string]: number } = {};
+  let fixedErrorsCounter = 0;
+
+  const totalErrors: { [key: string]: number } = {};
+  let totalErrorsCounter = 0;
+
+  report.items.forEach((item) => {
+    const fixed = item.fixed ? 1 : 0;
+    fixedErrors[item.code] = (fixedErrors[item.code] || 0) + fixed;
+    fixedErrorsCounter += fixed;
+    totalErrors[item.code] = (totalErrors[item.code] || 0) + 1;
+    totalErrorsCounter += 1;
+  });
 
   // Add statistic info to summary
   report.summary = {
     ...report.summary,
     ...{
       timestamp: new Date().toISOString(),
-      cumulativeErrors: report.items.length,
-      uniqueErrors: uniqueErrors.length,
-      uniqueErrorsList: uniqueErrors,
-      autofixedErrors: autofixedErrors.length,
-      autofixedErrorsList: autofixedErrors,
+      uniqueErrors: Object.keys(totalErrors).length,
+      totalErrors: totalErrorsCounter,
+      totalErrorsList: totalErrors,
+      fixedErrors: fixedErrorsCounter,
+      fixedErrorsList: fixedErrors,
       files: fileNames.length,
       filesList: fileNames,
     },
