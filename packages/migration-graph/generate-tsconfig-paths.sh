465188329bf7405229b2eb37309ead4b2cc36151
Marc Lynch <lynchmarc2@gmail.com>
2022-08-29 15:03:41 -0500

    refactor: move migration-graph into rehearsal

--- packages/migration-graph/generate-tsconfig-paths.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run the script to build tsconfig paths
-./node_modules/.bin/node dist/generate-tsconfig-paths.ts
-
-# run prettier to fix formatting issues introduced by jsonc-parser
-./node_modules/.bin/prettier --write 'tsconfig.base.json'
