d75335922680824da61dd9aba37496c261171841
egorio <justegorio@gmail.com>
2022-08-31 20:44:04 -0700

    chore: Figuring out how to add action package to the repo with test

---
+++ packages/action/src/index.ts
@@ -0,0 +1,1 @@
+console.log('Action Test');
---
+++ packages/action/test/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Test action', function () {
+  test('run', async () => {
+    expect(true).toBeTruthy();
+  });
+});
