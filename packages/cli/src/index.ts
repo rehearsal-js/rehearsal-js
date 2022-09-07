43a4b8cacb408093a842430b902a03f7fae6b4dc
everbits <everbits@linkedin.com>
2022-09-06 22:45:00 -0700

    chore: Avoid using `executableFile` to make command compilable for GitHub action

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,21 +1,16 @@
 import { Command } from 'commander';
 import { version } from '../package.json';
-import { join } from 'path';
+
+import { upgradeCommand } from "./commands/upgrade";
+import { migrateCommand } from "./commands/migrate";
 
 const program = new Command();
 
 program
   .name('rehearsal')
   .version(version)
-  .command(
-    'upgrade',
-    'Upgrade typescript dev-dependency with compilation insights and auto-fix options',
-    {
-      executableFile: join(__dirname, './commands/upgrade'),
-    }
-  )
-  .command('migrate', 'Migrate Javascript project to Typescript', {
-    executableFile: join(__dirname, './commands/migrate'),
-  });
+  .addCommand(upgradeCommand)
+  .addCommand(migrateCommand)
+  .parse(process.argv);
 
 export { program as cli };
