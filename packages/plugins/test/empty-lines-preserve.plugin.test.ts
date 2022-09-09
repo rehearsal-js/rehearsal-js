ba4b8db1bd1b07b7befda0c3f18a29699e76b1fe
Chad Hietala <chadhietala@gmail.com>
2022-09-09 11:02:47 -0400

    chore: Use Snapshots and Fixturify Project (#359)

--- packages/plugins/test/empty-lines-preserve.plugin.test.ts
+++ packages/plugins/test/empty-lines-preserve.plugin.test.ts
@@ -1,33 +1,64 @@
 import { RehearsalService } from '@rehearsal/service';
-import { cleanFixturesFiles, createFixturesFromTemplateFiles } from '@rehearsal/utils';
-import { readFileSync } from 'fs';
+import { Project } from 'fixturify-project';
 import { resolve } from 'path';
-import { afterEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 
 import { EmptyLinesPreservePlugin } from '../src';
 
 describe('Test EmptyLinesPreservePlugin', function () {
-  const fixturesPath = resolve(__dirname, 'fixtures', 'empty-lines-preserve');
+  let project: Project;
+
+  beforeEach(() => {
+    project = new Project('foo', '0.0.0');
+    delete project.files['index.js'];
+  });
 
   afterEach(() => {
-    cleanFixturesFiles(fixturesPath, fixturesPath);
+    project.dispose();
   });
 
   test('run', async () => {
-    const files = createFixturesFromTemplateFiles(fixturesPath, fixturesPath);
-    const fileNames = files.map((file) => resolve(fixturesPath, file));
-    const service = new RehearsalService({ baseUrl: fixturesPath }, fileNames);
+    project.files['index.ts'] = `
+// This is a single line comment
+
+// This is another single line comment
+const someTrueVariable = true;
+
+/*
+* This is multiline comment
+*
+
+  With some empty lines it it
+*/
+const someFalseVariable = false;
+
+
+
+/**
+ * This is a JSDoc comment
+ *
+
+* With some empty lines it it
+*/
+function someFunction(): void {
+
+  return ;
+}
+    `;
+
+    project.write();
+
+    const files = Object.keys(project.files).map((file) => resolve(project.baseDir, file));
+    const service = new RehearsalService({ baseUrl: project.baseDir }, files);
 
     const plugin = new EmptyLinesPreservePlugin(service);
 
-    expect(fileNames).toHaveLength(1);
-
-    for (const fileName of fileNames) {
+    for (const fileName of files) {
       const result = await plugin.run({ fileName });
       const resultText = service.getFileText(fileName).trim();
 
       expect(result).toHaveLength(1);
-      expect(resultText).toEqual(readFileSync(`${fileName}.output`).toString().trim());
+      expect(resultText).toMatchSnapshot();
     }
   });
 });
--- packages/plugins/test/empty-lines-restore.plugin.test.ts
+++ packages/plugins/test/empty-lines-restore.plugin.test.ts
@@ -1,33 +1,64 @@
 import { RehearsalService } from '@rehearsal/service';
-import { cleanFixturesFiles, createFixturesFromTemplateFiles } from '@rehearsal/utils';
-import { readFileSync } from 'fs';
+import { Project } from 'fixturify-project';
 import { resolve } from 'path';
-import { afterEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 
 import { EmptyLinesRestorePlugin } from '../src';
 
 describe('Test EmptyLinesRestorePlugin', function () {
-  const fixturesPath = resolve(__dirname, 'fixtures', 'empty-lines-restore');
+  let project: Project;
+
+  beforeEach(() => {
+    project = new Project('foo', '0.0.0');
+    delete project.files['index.js'];
+  });
 
   afterEach(() => {
-    cleanFixturesFiles(fixturesPath, fixturesPath);
+    project.dispose();
   });
 
   test('run', async () => {
-    const files = createFixturesFromTemplateFiles(fixturesPath, fixturesPath);
-    const fileNames = files.map((file) => resolve(fixturesPath, file));
-    const service = new RehearsalService({ baseUrl: fixturesPath }, fileNames);
+    project.files['index.ts'] = `
+// This is a single line comment
+//:line:
+// This is another single line comment
+  const someTrueVariable = true;
+//:line:
+/*
+  * This is multiline comment
+  *
+    //:line:
+    With some empty lines it it
+  */
+const someFalseVariable = false;
+//:line:
+//:line:
+//:line:
+/**
+ * This is a JSDoc comment
+ *
+//:line:
+  * With some empty lines it it
+  */
+function someFunction(): void {
+  //:line:
+  return ;
+}
+    `;
+    project.write();
+
+    const fileNames = Object.keys(project.files).map((file) => resolve(project.baseDir, file));
+
+    const service = new RehearsalService({ baseUrl: project.baseDir }, fileNames);
 
     const plugin = new EmptyLinesRestorePlugin(service);
-
-    expect(fileNames).toHaveLength(1);
 
     for (const fileName of fileNames) {
       const result = await plugin.run({ fileName });
       const resultText = service.getFileText(fileName).trim();
 
       expect(result).toHaveLength(1);
-      expect(resultText).toEqual(readFileSync(`${fileName}.output`).toString().trim());
+      expect(resultText).toMatchSnapshot();
     }
   });
 });
