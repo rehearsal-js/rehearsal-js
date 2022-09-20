0ba15e92aa5a4ee4e9e8c94b20d5d2124cbb7d3f
Brenda Huang <brehuang@linkedin.com>
2022-09-20 10:29:36 -0700

    chore: add more tests to reporter class

--- packages/reporter/test/reporter.test.ts
+++ packages/reporter/test/reporter.test.ts
@@ -1,120 +1,103 @@
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync, rmSync } from 'fs';
 import { resolve } from 'path';
-import { assert, describe, expect, test } from 'vitest';
+import { DiagnosticWithLocation, SourceFile } from 'typescript';
+import { afterEach, assert, beforeEach, describe, expect, test } from 'vitest';
+import { mock } from 'vitest-mock-extended';
 
-import { Report, Reporter } from '../src';
+import { jsonFormatter, mdFormatter, type Report, Reporter } from '../src';
 
 describe('Test reporter', function () {
-  const basePath = resolve(__dirname, 'fixtures');
+  const basePath = resolve(__dirname, 'fixtures/reporter');
+
+  let reporter: Reporter | undefined;
+
+  beforeEach(() => {
+    reporter = new Reporter('test', basePath).load(resolve(basePath, 'rehearsal-report.json'));
+  });
+
+  afterEach(() => {
+    reporter = undefined;
+  });
 
   test('load', async () => {
-    const reporter = new Reporter('test', basePath).load(
-      resolve(basePath, '.rehearsal-report.input.json')
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const report = (reporter as any).report as Report;
 
     expect(report.summary.basePath).toMatch(/base/);
     expect(report.summary.timestamp).toMatch(/\d+/);
-
-    assert.deepEqual(report, expectedReport(basePath, '.rehearsal-report.output.json'));
+    expect(report).toMatchSnapshot();
   });
 
   test('save', async () => {
-    // TODO: Implement test case
+    const testSaveFile = resolve(basePath, 'test-save.json');
+
+    reporter!.save(testSaveFile);
+    expect(existsSync(testSaveFile)).toBeTruthy;
+    expect(readFileSync(testSaveFile, 'utf-8')).toMatchSnapshot();
+
+    rmSync(testSaveFile);
   });
 
   test('print, json', async () => {
-    // TODO: Implement test case
+    const testPrintJsonFile = resolve(basePath, 'test-print-json.json');
+
+    reporter!.print(testPrintJsonFile, jsonFormatter);
+    expect(existsSync(testPrintJsonFile)).toBeTruthy;
+    expect(readFileSync(testPrintJsonFile, 'utf-8')).toMatchSnapshot();
+
+    rmSync(testPrintJsonFile);
   });
 
   test('print, pull-request-md', async () => {
-    // TODO: Implement test case
+    const testPrintMdFile = resolve(basePath, 'test-print-md.md');
+
+    reporter!.print(testPrintMdFile, mdFormatter);
+    expect(existsSync(testPrintMdFile)).toBeTruthy;
+    expect(readFileSync(testPrintMdFile, 'utf-8')).toMatchSnapshot();
+
+    rmSync(testPrintMdFile);
   });
 
   test('addItem', async () => {
-    // TODO: Implement test case
+    const mockSourceFile = mock<SourceFile>();
+    mockSourceFile.fileName = 'testFile.ts';
+    mockSourceFile.getLineAndCharacterOfPosition.mockReturnValue({ line: 0, character: 5 });
+
+    const mockDiagnostic = mock<DiagnosticWithLocation>();
+    mockDiagnostic.file = mockSourceFile;
+    mockDiagnostic.category = 1;
+    mockDiagnostic.messageText = 'unused variable';
+    mockDiagnostic.code = 1000;
+
+    const files = {
+      '/base/path/testFile.ts': {
+        fileName: '/base/path/testFile.ts',
+        location: { line: 3, character: 7 },
+        fixed: false,
+        code: undefined,
+        codeFixAction: undefined,
+        hint: 'This is the hint.',
+        hintAdded: true,
+        roles: ['analysisTarget' as const, 'unmodified' as const],
+      },
+    };
+
+    reporter!.addItem(mockDiagnostic, files, false, undefined);
+
+    const testAddItemFile = resolve(basePath, 'test-add-item.json');
+
+    reporter!.save(testAddItemFile);
+    expect(existsSync(testAddItemFile)).toBeTruthy;
+    expect(readFileSync(testAddItemFile, 'utf-8')).toMatchSnapshot();
+
+    rmSync(testAddItemFile);
   });
 
   test('getFileNames', async () => {
-    const reporter = new Reporter('test', basePath).load(
-      resolve(basePath, '.rehearsal-report.input.json')
-    );
-
-    assert.deepEqual(reporter.getFileNames(), ['first.ts', 'second.ts']);
+    assert.deepEqual(reporter!.getFileNames(), ['/base/path/file1.ts']);
   });
 
   test('getItemsByFile', async () => {
-    const reporter = new Reporter('test', basePath).load(
-      resolve(basePath, '.rehearsal-report.input.json')
-    );
-
-    assert.deepEqual(reporter.getItemsByAnalysisTarget('first.ts'), [
-      {
-        category: 'Error',
-        fixedFiles: [
-          {
-            fileName: 'first.ts',
-            location: {
-              line: 0,
-              character: 0,
-            },
-          },
-        ],
-        commentedFiles: [],
-        code: 6133,
-        analysisTarget: 'first.ts',
-        fixed: true,
-        hint: "The declaration 'fs' is never read or used. Remove the declaration or use it.",
-        nodeLocation: {
-          character: 0,
-          length: 20,
-          line: 0,
-          start: 0,
-        },
-        message: "'fs' is declared but its value is never read.",
-        nodeKind: 'ImportDeclaration',
-        nodeText: "import fs from 'fs';",
-      },
-    ]);
-
-    assert.deepEqual(reporter.getItemsByAnalysisTarget('second.ts'), [
-      {
-        category: 'Error',
-        fixedFiles: [
-          {
-            fileName: 'second.ts',
-            location: {
-              line: 0,
-              character: 0,
-            },
-          },
-        ],
-        commentedFiles: [],
-        code: 6133,
-        analysisTarget: 'second.ts',
-        fixed: true,
-        hint: "The declaration 'parse' is never read or used. Remove the declaration or use it.",
-        nodeLocation: {
-          character: 18,
-          length: 5,
-          line: 0,
-          start: 18,
-        },
-        message: "'parse' is declared but its value is never read.",
-        nodeKind: 'ImportSpecifier',
-        nodeText: 'parse',
-      },
-    ]);
+    const items = reporter!.getItemsByAnalysisTarget('/base/path/file1.ts');
+    expect(items).toMatchSnapshot();
   });
 });
-
-/**
- * Prepares the report to compare with.
- */
-function expectedReport(basePath: string, fileName: string): Report {
-  const content = readFileSync(resolve(basePath, fileName)).toString();
-
-  return JSON.parse(content) as Report;
-}
