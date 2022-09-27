b57054d0a4ea92de45d8ae9b391f51d4430bb944
Matthew Edwards <medwards@linkedin.com>
2022-09-27 09:28:15 -0700

    Add fixture for ember app with service

--- packages/migration-graph/tests/fixtures/scenarios.test.ts
+++ packages/migration-graph/tests/fixtures/scenarios.test.ts
@@ -1,6 +1,6 @@
-import { PreparedApp, Scenario } from 'scenario-tester';
+import { PreparedApp, Scenario, Scenarios } from 'scenario-tester';
 import { beforeAll, beforeEach, describe, expect, test } from 'vitest';
-import { addonScenarios, appScenarios, clean, setup } from './scenarios';
+import { addonScenarios, appScenarios, clean, getEmberApp, setup } from './scenarios';
 const TEST_TIMEOUT = 500000;
 
 // Enable these tests to validate if our scenarios fixtures build a funciton all
@@ -8,6 +8,23 @@ describe.skip('scenarios [internal] validate file fixture projects with acceptan
   beforeAll(() => {
     setup();
   });
+
+  test.skip(
+    'debug scenario test',
+    async () => {
+      const app = await getEmberApp('app'); // Choose a scenario to debug
+      clean(app.dir); // Remove node_modules to ensure it's regenerated between scenarios
+      console.log(app.dir);
+
+      // Setup
+      let result = await app.execute('volta run yarn install');
+      expect(result.exitCode, result.output).toBe(0);
+      // Should pass acceptance tests
+      result = await app.execute('volta run yarn test');
+      expect(result.exitCode, result.output).toBe(0);
+    },
+    TEST_TIMEOUT
+  );
 
   describe('app-variants', () => {
     appScenarios.forEachScenario((scenario: Scenario) => {
