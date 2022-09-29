ec9bcbe6f85817c1e8f12b19b14de2c586d054df
Matthew Edwards <medwards@linkedin.com>
2022-09-29 12:39:21 -0700

    feat: discover ember service dependency information from a file

---
+++ packages/migration-graph/src/lib/discover-ember-service-dependencies.ts
@@ -0,0 +1,221 @@
+import type {
+  CallExpression,
+  ClassDeclaration,
+  ClassExpression,
+  ClassMember,
+  ClassProperty,
+  Decorator,
+  ExportDefaultDeclaration,
+  Identifier,
+  ImportDeclaration,
+  ImportSpecifier,
+  ModuleItem,
+  NamedImportSpecifier,
+  StringLiteral,
+} from '@swc/core';
+import { parseFileSync } from '@swc/core';
+import { join } from 'path';
+
+type EmberInferredServiceDependency = {
+  addonName?: string;
+  serviceName: string;
+};
+
+const EMPTY_RESULT: EmberInferredServiceDependency[] = [];
+
+export function discoverServiceDependencies(
+  baseDir: string,
+  pathToFile: string
+): EmberInferredServiceDependency[] {
+  const filePath = join(baseDir, pathToFile);
+
+  const parsed = parseFileSync(filePath, { syntax: 'ecmascript', decorators: true });
+
+  const maybeImportDeclaration = findImportDeclarationsBySource(parsed.body, '@ember/service');
+
+  if (!maybeImportDeclaration) {
+    console.log('No import declaration found');
+    return EMPTY_RESULT;
+  }
+
+  const importDeclaration = maybeImportDeclaration as ImportDeclaration;
+
+  // In the case of re-assignment from ineject as service, we walk the specifiers
+  const maybeSpecifier = findSpecifierByExportedName(importDeclaration.specifiers, 'inject');
+
+  if (!maybeSpecifier) {
+    return EMPTY_RESULT;
+  }
+
+  const decoratorName = maybeSpecifier.local.value;
+
+  if (!decoratorName) {
+    return EMPTY_RESULT;
+  }
+
+  let foundClasses: Array<ClassExpression | ClassDeclaration> = [];
+
+  // Find all ClassDeclaration in body of the parsed file
+  const classDeclrations = findClassDeclarations(parsed.body);
+
+  if (classDeclrations) {
+    foundClasses = foundClasses.concat(Array.from(classDeclrations));
+  }
+
+  // Typical use case will have ExportDefaultDeclaration where the body is a ClassExpression
+  const someDefaultExport = parsed.body.find(
+    (m: ModuleItem) => m.type == 'ExportDefaultDeclaration'
+  );
+
+  if (someDefaultExport) {
+    const edd = someDefaultExport as ExportDefaultDeclaration;
+    // Check if the ExportDefaultDeclaration has a ClassExpression
+    if (edd.decl.type === 'ClassExpression') {
+      foundClasses.push(edd.decl as ClassExpression);
+    } else {
+      console.log('Default export does not contain a ClassExpressio');
+    }
+  }
+
+  if (foundClasses.length < 1) {
+    throw new Error('Unexpected Error: No classes found despite having imported @ember/service.');
+  }
+
+  let results: EmberInferredServiceDependency[] = EMPTY_RESULT;
+
+  for (const classExp of foundClasses) {
+    const body = classExp.body;
+
+    const classProperties = filterByClassPropertyWithDecorators(body);
+
+    if (!classProperties) {
+      continue;
+    }
+
+    const classPropertiesWithDecorators = filterClassPropertyWithDecoratorName(
+      classProperties,
+      decoratorName
+    );
+
+    if (!classPropertiesWithDecorators) {
+      continue;
+    }
+
+    results = classPropertiesWithDecorators.map((prop: ClassProperty) => {
+      if (prop.decorators) {
+        const maybeDecorator = findDecoratorWithName(prop.decorators, decoratorName);
+
+        if (!!maybeDecorator && isDecoratorWithExpressionTypeCallExpression(maybeDecorator)) {
+          const callExpression = maybeDecorator.expression as CallExpression;
+          const arg = callExpression.arguments[0].expression as StringLiteral;
+          // someAddon@serviceName
+
+          return parseServiceMetaFromString(arg.value);
+        }
+      }
+
+      // Fallback if decorator.expression where decoratorName`  is not call expression
+      return { serviceName: (prop.key as Identifier).value };
+    });
+  }
+
+  return results;
+}
+
+function findClassDeclarations(items: ModuleItem[]): Array<ClassDeclaration> | undefined {
+  return items
+    .filter((i: ModuleItem) => i.type == 'ClassDeclaration')
+    .map((i) => i as ClassDeclaration);
+}
+
+function parseServiceMetaFromString(str: string): EmberInferredServiceDependency {
+  if (str.includes('@')) {
+    const [addonName, serviceName] = str.split('@');
+    return { addonName, serviceName };
+  }
+  return { serviceName: str };
+}
+
+function filterByClassPropertyWithDecorators(members: ClassMember[]): ClassProperty[] {
+  const result: unknown = members.filter(
+    (m: ClassMember) => m.type == 'ClassProperty' && !!m.decorators
+  );
+  return result as ClassProperty[];
+}
+
+function filterClassPropertyWithDecoratorName(
+  classProperties: ClassProperty[],
+  decoratorName: string
+): ClassProperty[] {
+  return classProperties.filter((prop: ClassProperty) => {
+    const decorators = prop.decorators;
+    return !!decorators && hasDecoratorWithName(decorators, decoratorName);
+  });
+}
+
+function findDecoratorWithName(
+  decorators: Decorator[],
+  decoratorName: string
+): Decorator | undefined {
+  return decorators.find((d) => {
+    let identifier: Identifier | undefined;
+
+    // For a given decorate it's expression proeprty can either
+    // be an Identifier or CallExpression
+
+    // If it's an identifier it would look like
+    // e.g. `@service name;`
+    if (isDecoratorWithExpressionTypeIdenfitier(d)) {
+      identifier = d.expression as Identifier;
+    }
+
+    // If it's a CallExpression this is the case where we pass meta data
+    // into the decorator
+    // e.g. `@service('someAddon@serviceName') myVariable;`
+    else if (isDecoratorWithExpressionTypeCallExpression(d)) {
+      const callExpression = d.expression as CallExpression;
+      identifier = callExpression.callee as Identifier;
+    }
+
+    return !!identifier && identifier.value === decoratorName;
+  });
+}
+
+function hasDecoratorWithName(decorators: Decorator[], decoratorName: string): boolean {
+  return !!findDecoratorWithName(decorators, decoratorName);
+}
+
+function isDecoratorWithExpressionTypeIdenfitier(d: Decorator): boolean {
+  return d.expression.type === 'Identifier';
+}
+
+function isDecoratorWithExpressionTypeCallExpression(d: Decorator): boolean {
+  return d.expression.type === 'CallExpression';
+}
+
+function findImportDeclarationsBySource(
+  items: ModuleItem[],
+  source: string
+): ImportDeclaration | undefined {
+  const item = items.find((i) => isImportDeclaration(i, source));
+  return item ? (item as ImportDeclaration) : undefined;
+}
+
+function isImportDeclaration(m: ModuleItem, source: string): boolean {
+  return m.type === 'ImportDeclaration' && m.source.value === source;
+}
+
+function findSpecifierByExportedName(
+  specifiers: ImportSpecifier[],
+  exportName: string
+): ImportSpecifier | undefined {
+  return specifiers.find((s: NamedImportSpecifier | ImportSpecifier) => {
+    // If imported is null, then only have a local value, which implies no re-assignment.
+    // e.g.
+    // import { foo as bar } from 'baz'; -> bar is local, foo is imported
+    // import { foo } from 'baz'; -> foo is local, imported is null
+    return (
+      (s as NamedImportSpecifier)?.imported?.value === exportName || s.local.value === exportName
+    );
+  });
+}
