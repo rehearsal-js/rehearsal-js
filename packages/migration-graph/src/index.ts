09e50bec19812c531386a08e0c540cac1a6c6a32
Matthew Edwards <medwards@linkedin.com>
2022-10-28 17:13:21 -0700

    chore: remove module-resolution util API
    - module-resolution.ts was a util for the basic dependency
      graph written with SWC.
    - If we need it remove dep-cruiser we should bring this back.

--- packages/migration-graph/src/index.ts
+++ packages/migration-graph/src/index.ts
@@ -4,14 +4,6 @@ export { GraphNode } from './utils/graph-node';
 export { Graph } from './utils/graph';
 
 export {
-  isModuleRelative,
-  isModuleNonRelative,
-  isDirectoryPackage,
-  getMainEntrypoint,
-  resolveRelativeModule,
-} from './module-resolution';
-
-export {
   getMigrationStrategy,
   type MigrationStrategy,
   type MigrationStrategyOptions,
