2f2cf27a69fe39cb09b802da1bba31c82647b019
Matthew Edwards <medwards@linkedin.com>
2022-11-07 14:32:05 -0800

    chore: test TODO for discover-ember-packages

--- packages/migration-graph-ember/tests/utils/discover-ember-packages.test.ts
+++ packages/migration-graph-ember/tests/utils/discover-ember-packages.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
+// TODO: should have a test
 describe('discover-ember-packages', () => {
   test.todo('should have test', () => {
     expect(true).toBe(false);
