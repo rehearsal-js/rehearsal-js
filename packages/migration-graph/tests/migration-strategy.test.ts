2bc1eec7ea918bb3181167e5ad9d750d69c8d7b2
Matthew Edwards <medwards@linkedin.com>
2022-11-07 15:14:58 -0800

    chore: move test timeout to config

--- packages/migration-graph/tests/migration-strategy.test.ts
+++ packages/migration-graph/tests/migration-strategy.test.ts
@@ -7,8 +7,6 @@ import {
 import { SourceType } from '../src/source-type';
 import { getMigrationStrategy, SourceFile } from '../src/migration-strategy';
 
-const TEST_TIMEOUT = 500000;
-
 describe('migration-strategy', () => {
   describe('package', () => {
     test('simple', () => {
@@ -36,75 +34,59 @@ describe('migration-strategy', () => {
       'app/components/salutation.js',
       'app/router.js',
     ];
-    test(
-      'app should match migration order',
-      async () => {
-        const project = await getEmberProjectFixture('app');
+    test('app should match migration order', async () => {
+      const project = await getEmberProjectFixture('app');
 
-        const strategy = getMigrationStrategy(project.baseDir);
-        const files: Array<SourceFile> = strategy.getMigrationOrder();
-        const actual: Array<string> = files.map((f) => f.relativePath);
-        expect(actual).toStrictEqual(EXPECTED_APP_FILES);
-        expect(strategy.sourceType).toBe(SourceType.EmberApp);
-      },
-      TEST_TIMEOUT
-    );
+      const strategy = getMigrationStrategy(project.baseDir);
+      const files: Array<SourceFile> = strategy.getMigrationOrder();
+      const actual: Array<string> = files.map((f) => f.relativePath);
+      expect(actual).toStrictEqual(EXPECTED_APP_FILES);
+      expect(strategy.sourceType).toBe(SourceType.EmberApp);
+    });
 
-    test(
-      'app-with-in-repo-addon should match migration order',
-      async () => {
-        const project = await getEmberProjectFixture('app-with-in-repo-addon');
+    test('app-with-in-repo-addon should match migration order', async () => {
+      const project = await getEmberProjectFixture('app-with-in-repo-addon');
 
-        const strategy = getMigrationStrategy(project.baseDir);
-        const files: Array<SourceFile> = strategy.getMigrationOrder();
-        const actual: Array<string> = files.map((f) => f.relativePath);
-        expect(actual).toStrictEqual([
-          'lib/some-addon/addon/components/greet.js',
-          'lib/some-addon/app/components/greet.js',
-          'lib/some-addon/index.js',
-          ...EXPECTED_APP_FILES,
-        ]);
-        expect(strategy.sourceType).toBe(SourceType.EmberApp);
-      },
-      TEST_TIMEOUT
-    );
+      const strategy = getMigrationStrategy(project.baseDir);
+      const files: Array<SourceFile> = strategy.getMigrationOrder();
+      const actual: Array<string> = files.map((f) => f.relativePath);
+      expect(actual).toStrictEqual([
+        'lib/some-addon/addon/components/greet.js',
+        'lib/some-addon/app/components/greet.js',
+        'lib/some-addon/index.js',
+        ...EXPECTED_APP_FILES,
+      ]);
+      expect(strategy.sourceType).toBe(SourceType.EmberApp);
+    });
 
-    test(
-      'app-with-in-repo-engine should match migration order',
-      async () => {
-        const project = await getEmberProjectFixture('app-with-in-repo-engine');
+    test('app-with-in-repo-engine should match migration order', async () => {
+      const project = await getEmberProjectFixture('app-with-in-repo-engine');
 
-        const strategy = getMigrationStrategy(project.baseDir);
-        const files: Array<SourceFile> = strategy.getMigrationOrder();
-        const actual: Array<string> = files.map((f) => f.relativePath);
-        expect(actual).toStrictEqual([
-          'lib/some-engine/addon/resolver.js',
-          'lib/some-engine/addon/engine.js',
-          'lib/some-engine/addon/routes.js',
-          'lib/some-engine/index.js',
-          ...EXPECTED_APP_FILES,
-        ]);
-        expect(strategy.sourceType).toBe(SourceType.EmberApp);
-      },
-      TEST_TIMEOUT
-    );
+      const strategy = getMigrationStrategy(project.baseDir);
+      const files: Array<SourceFile> = strategy.getMigrationOrder();
+      const actual: Array<string> = files.map((f) => f.relativePath);
+      expect(actual).toStrictEqual([
+        'lib/some-engine/addon/resolver.js',
+        'lib/some-engine/addon/engine.js',
+        'lib/some-engine/addon/routes.js',
+        'lib/some-engine/index.js',
+        ...EXPECTED_APP_FILES,
+      ]);
+      expect(strategy.sourceType).toBe(SourceType.EmberApp);
+    });
 
-    test(
-      'addon should match migration order',
-      async () => {
-        const project = await getEmberProjectFixture('addon');
+    test('addon should match migration order', async () => {
+      const project = await getEmberProjectFixture('addon');
 
-        const strategy = getMigrationStrategy(project.baseDir);
-        const files: Array<SourceFile> = strategy.getMigrationOrder();
-        const actual: Array<string> = files.map((f) => f.relativePath);
-        expect(actual).toStrictEqual([
-          'addon/components/greet.js',
-          'app/components/greet.js',
-          'index.js',
-        ]);
-        expect(strategy.sourceType).toBe(SourceType.EmberAddon);
-      },
-      TEST_TIMEOUT
-    );
+      const strategy = getMigrationStrategy(project.baseDir);
+      const files: Array<SourceFile> = strategy.getMigrationOrder();
+      const actual: Array<string> = files.map((f) => f.relativePath);
+      expect(actual).toStrictEqual([
+        'addon/components/greet.js',
+        'app/components/greet.js',
+        'index.js',
+      ]);
+      expect(strategy.sourceType).toBe(SourceType.EmberAddon);
+    });
   });
 });
