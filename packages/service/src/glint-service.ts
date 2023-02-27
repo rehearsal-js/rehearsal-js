7c6d28cedd470d0f1fa5719e29d3eef636cc1e2c
Brenda Huang <brehuang@linkedin.com>
2023-02-27 12:17:27 -0800

    feat: fix test

--- packages/service/src/glint-service.ts
+++ packages/service/src/glint-service.ts
@@ -1,24 +1,39 @@
-import { analyzeProject } from '@glint/core';
-import { Diagnostic } from 'typescript';
+import { analyzeProject, ProjectAnalysis } from '@glint/core';
+// import { Diagnostic } from 'typescript';
 
-type GlintDiagnostic = Diagnostic & { isGlintTransformDiagnostic?: boolean };
-type TransformManager = {
-  getTransformDiagnostics: (fileName: string) => GlintDiagnostic[];
+// type GlintDiagnostic = Diagnostic & { isGlintTransformDiagnostic?: boolean };
+type GlintLanguageServer = ProjectAnalysis['languageServer'];
+declare type Diagnostic = {
+  range: {
+    start: {
+      line: number;
+      character: number;
+    }
+    end: {
+      line: number;
+      character: number;
+    }
+  };
+  severity?: 1 | 2 | 3 | 4;
+  code?: number | string;
+  codeDescription?: { href: string};
+  source?: string;
+  message: string;
 };
+// type TransformManager = {
+//   getTransformDiagnostics: (fileName: string) => GlintDiagnostic[];
+// };
 export class RehearsalGlintService {
   private rootDir: string;
-  private glintParser?: TransformManager;
+  private glintParser?: GlintLanguageServer;
   constructor(projectDirectory: string) {
     this.rootDir = projectDirectory;
   }
-  getGlintDiagnostics(fileName: string): GlintDiagnostic[] {
-    console.log('filename', fileName);
+  getGlintDiagnostics(fileName: string): Diagnostic[] {
     if (!this.glintParser) {
-      console.log('not glintParser');
-      const { transformManager } = analyzeProject(this.rootDir);
-      this.glintParser = transformManager;
+      const { languageServer } = analyzeProject(this.rootDir);
+      this.glintParser = languageServer;
     }
-    console.log('this.glintParser', this.glintParser.getTransformDiagnostics);
-    return this.glintParser.getTransformDiagnostics(fileName);
+    return this.glintParser.getDiagnostics(fileName);
   }
 }
--- packages/service/test/glint.test.ts
+++ packages/service/test/glint.test.ts
@@ -1,19 +1,10 @@
-import { readJSONSync } from 'fs-extra';
 import { resolve, dirname } from 'path';
 import { describe, test, beforeEach } from 'vitest';
 import { dirSync, setGracefulCleanup, DirResult } from 'tmp';
 import { Project } from 'fixturify-project';
 
-import {
-  // CompilerOptions,
-  // DiagnosticWithLocation,
-  findConfigFile,
-  parseJsonConfigFileContent,
-  readConfigFile,
-  sys,
-} from 'typescript';
 import { getGlintProject } from '@rehearsal/test-support/src/project';
-import { RehearsalService } from '../src';
+import { RehearsalGlintService } from '../src/glint-service';
 
 // setGracefulCleanup();
 
@@ -37,18 +28,18 @@ describe('Test service', function () {
 
   test('should pass', () => {
     console.log('basePath', basePath);
-    const configFile = findConfigFile(basePath, sys.fileExists, 'tsconfig.json');
-    if (!configFile) {
-      // throw Error('configFile not found');
-      console.log('configFile not found');
-      return;
-    }
-    const { config } = readConfigFile(configFile, sys.readFile);
+    // const configFile = findConfigFile(basePath, sys.fileExists, 'tsconfig.json');
+    // if (!configFile) {
+    //   // throw Error('configFile not found');
+    //   console.log('configFile not found');
+    //   return;
+    // }
+    // const { config } = readConfigFile(configFile, sys.readFile);
 
-    const parsed = parseJsonConfigFileContent(config, sys, dirname(configFile), {}, configFile);
-    console.log('fileNames', parsed.fileNames);
+    // const parsed = parseJsonConfigFileContent(config, sys, dirname(configFile), {}, configFile);
+    // console.log('fileNames', parsed.fileNames);
 
-    const service = new RehearsalService(parsed.options, parsed.fileNames, basePath);
+    const service = new RehearsalGlintService(basePath);
     // let arr = [];
     // for (const file of parsed.fileNames) {
     //   const diagnostics = service.getGlintDiagnostics(file);
