9e859c120f2bdb56f0ab9e4108bf3b76fe5de71d
egorio <justegorio@gmail.com>
2023-03-09 12:44:38 -0800

    feat: prettier plugin + fix for the cli upgrade test

---
+++ packages/plugins/test/lint.plugin.test.ts
@@ -0,0 +1,41 @@
+import { resolve } from 'path';
+import { describe, expect, test } from 'vitest';
+import { LintPlugin } from '../src/index.js';
+import { initProject, mockPluginRunnerContext } from './utils.js';
+
+describe('Test LintPlugin', () => {
+  test('run', async () => {
+    const project = await initProject('prettier-test', {
+      'index.js': './test/fixtures/lint.ts.fixture',
+    });
+
+    const context = mockPluginRunnerContext(project);
+
+    const plugin = new LintPlugin();
+
+    for (const file in project.files) {
+      const fileName = resolve(project.baseDir, file);
+
+      const result = await plugin.run(fileName, context, {
+        reportErrors: true,
+        eslintOptions: {
+          useEslintrc: false,
+          fix: true,
+          baseConfig: {
+            extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+          },
+        },
+      });
+
+      const resultText = context.rehearsal.getFileText(fileName).trim();
+
+      expect(result).toHaveLength(1);
+      expect(resultText).toMatchSnapshot();
+
+      expect(context.reporter.currentRun.items).toHaveLength(1);
+      expect(context.reporter.currentRun.items).toMatchSnapshot();
+    }
+
+    project.dispose();
+  });
+});
