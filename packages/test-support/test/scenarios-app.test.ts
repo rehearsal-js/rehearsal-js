80a37c4095f09199e7f7e641deb41d6d0b5d7505
Marc Lynch <lynchbomb@users.noreply.github.com>
2023-03-23 12:55:33 -0500

    test(test-support): remove hardcoded timeouts

--- packages/test-support/test/scenarios-app.test.ts
+++ packages/test-support/test/scenarios-app.test.ts
@@ -2,8 +2,6 @@ import { PreparedApp, Scenario } from 'scenario-tester';
 import { beforeEach, describe, expect, test } from 'vitest';
 import { appScenarios, clean } from '../src/scenarios.js';
 
-const TEST_TIMEOUT = 100000;
-
 // Enable these tests to validate if our scenarios fixtures build a funciton all
 describe('scenarios - app-variants', () => {
   appScenarios.forEachScenario((scenario: Scenario) => {
@@ -15,18 +13,14 @@ describe('scenarios - app-variants', () => {
         clean(app.dir); // Remove node_modules to ensure it's regenerated between scenarios
       });
 
-      test(
-        'should pass tests',
-        async () => {
-          // Setup
-          let result = await app.execute('npm install');
-          expect(result.exitCode, result.output).toBe(0);
-          // Should pass acceptance tests
-          result = await app.execute('npm test');
-          expect(result.exitCode, result.output).toBe(0);
-        },
-        TEST_TIMEOUT
-      );
+      test('should pass tests', async () => {
+        // Setup
+        let result = await app.execute('npm install');
+        expect(result.exitCode, result.output).toBe(0);
+        // Should pass acceptance tests
+        result = await app.execute('npm test');
+        expect(result.exitCode, result.output).toBe(0);
+      });
     });
   });
 });
