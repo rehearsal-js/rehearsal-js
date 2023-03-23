c930c9b8ef25d0a535efec625e2549511ce72eef
Chris Freeman <cfreeman.code@gmail.com>
2023-03-23 12:22:16 -0600

    fix: use service to read file text for diagnostics (#874)

---
+++ packages/migrate/test/fixtures/project/src/salutation.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class Salutation extends Component {
+  @service locale;
+  get name() {
+    if (this.locale.current() == 'en-US') {
+      return 'Bob';
+    }
+    return 'Unknown';
+  }
+}
