6658e27f8da5d4edd264b73c60298d123ae2bf89
Chad Hietala <chietala@linkedin.com>
2023-04-12 11:01:00 -0400

    fix: actually top sort the graph

---
+++ packages/migration-graph-ember/test/fixtures/monorepo/packages/d/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  moduleName() {
+    return 'd'
+  }
+}
--- packages/migration-graph-ember/test/utils/discover-ember-packages.test.ts
+++ packages/migration-graph-ember/test/utils/discover-ember-packages.test.ts
@@ -1,8 +1,37 @@
-import { describe, expect, test } from 'vitest';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { describe, expect, test, beforeEach, afterEach } from 'vitest';
+import { Project } from 'fixturify-project';
+import { EmberAppProjectGraph } from '../../src/index.js';
 
-// TODO: should have a test
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const FIXTURE_APP_PATH = resolve(__dirname, '../fixtures/monorepo');
+
 describe('discover-ember-packages', () => {
-  test.todo('should have test', () => {
-    expect(true).toBe(false);
+  let projectGraph: EmberAppProjectGraph;
+  let project: Project;
+  beforeEach(async () => {
+    project = Project.fromDir(FIXTURE_APP_PATH, { linkDeps: true, linkDevDeps: true });
+    project.addDevDependency('ember-source');
+    await project.write();
+    projectGraph = new EmberAppProjectGraph(project.baseDir);
+  });
+
+  afterEach(() => {
+    project.dispose();
+  });
+
+  test('returns the graph in top sort order', () => {
+    projectGraph.discover();
+    const nodes = projectGraph.graph.topSort();
+
+    expect(nodes.map((n) => n.content.pkg?.packageName)).toEqual([
+      '@company/b',
+      '@company/a',
+      '@company/c',
+      '@company/d',
+      'monorepo',
+    ]);
   });
 });
