b27674fff2d0f12630ef6826750df11999d19ffc
Chad Hietala <chadhietala@gmail.com>
2023-05-17 22:07:01 -0400

    chore: delete regen (#1057)

--- packages/regen/test/fixtures/src/test3.ts
+++
@@ -1,9 +0,0 @@
-function add(foo: number, bar: number): number {
-  return foo + bar;
-}
-
-// @ts-expect-error @rehearsal TODO TS2345: Argument of type 'string' is not assignable to parameter of type 'number'. Consider verifying both types, using type assertion: '("5" as string)', or using type guard: 'if ("5" instanceof string) { ... }'.
-add("5", 10);
-
-// @ts-expect-error @rehearsal TODO TS6133: The variable 'baz' is never read or used. Remove the variable or use it.
-const baz = "";
