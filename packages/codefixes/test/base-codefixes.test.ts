2fc105fd21ab6ccaa393e6178a87cc0c1656a1b7
egorio <justegorio@gmail.com>
2023-05-17 22:01:18 -0700

    chore: fix base-codefixes test

--- packages/codefixes/test/base-codefixes.test.ts
+++ packages/codefixes/test/base-codefixes.test.ts
@@ -1,71 +1,58 @@
 import { readdirSync, readFileSync } from 'node:fs';
 import { dirname, parse, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { Project } from 'fixturify-project';
 import { Reporter } from '@rehearsal/reporter';
 import { migrate } from '@rehearsal/migrate';
-import type fixturify from 'fixturify';
+import fixturify from "fixturify";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 describe('Test base codefixes', function () {
   const fixturesDir = resolve(__dirname, 'fixtures');
-  const codefixesDir = resolve(__dirname, '../src/fixes');
-  let project: Project;
+  const codefixesDir = resolve(__dirname, '../src/fixes/');
 
   // eg. ["addErrorTypeGuard","addMissingExport","addMissingTypesBasedOnInheritance","makeMemberOptional"]
   const transforms = readdirSync(codefixesDir).map((file) => parse(file).name);
 
-  beforeEach(() => {
-    project = Project.fromDir(fixturesDir, { linkDeps: true, linkDevDeps: true });
-  });
+  test.each(transforms)('%s', async (transform) => {
+    const project = Project.fromDir(fixturesDir, { linkDeps: true, linkDevDeps: true });
+    await project.write();
 
-  afterEach(() => {
-    project.dispose();
-  });
+    const dir = 'base-codefixes';
 
-  /**
-   * Compiles the project with LanguageService and apply autofix
-   * on corresponding diagnostic messages
-   */
-  async function runMigrate(basePath: string): Promise<void> {
-    const rootPath = resolve(project.baseDir);
+    const files = (project.files[dir] as fixturify.DirJSON)[transform] as fixturify.DirJSON;
+
+    expect(files, `Test files are missing for ${transform}`).toBeTypeOf('object');
+
+    const filesToMigrate = Object.keys(files).map((file) => resolve(project.baseDir, dir, transform, file));
 
     const reporter = new Reporter({
       tsVersion: '',
       projectName: '@rehearsal/test',
-      projectRootDir: rootPath,
+      projectRootDir: project.baseDir,
       commandName: '@rehearsal/migrate',
     });
 
-    await project.write();
-
-    migrate({
-      projectRootDir: rootPath,
-      packageDir: rootPath,
+    const input = {
+      projectRootDir: project.baseDir,
+      packageDir: project.baseDir,
+      filesToMigrate: filesToMigrate,
       reporter,
-      filesToMigrate: [resolve(basePath, 'index.ts')],
-    });
-  }
+    };
 
-  test.each(transforms)('%s', async (transform) => {
-    console.log(transform);
-    const migrateProjectDir = resolve(project.baseDir, transform);
+    for await (const _ of migrate(input)) {
+      // no ops
+    }
 
-    await runMigrate(migrateProjectDir);
-
-    expect(typeof project.files[transform] === 'object').toBe(true);
-    expect(project.files[transform] === null).toBe(false);
-
-    // SAFETY: the above assertions narrow the type to DirJSON.
-    const files = project.files[transform] as fixturify.DirJSON;
-
-    for (const input of Object.keys(files)) {
-      const actualOutput = readFileSync(resolve(migrateProjectDir, input), 'utf-8');
+    for (const file of filesToMigrate) {
+      const actualOutput = readFileSync(file, 'utf-8');
 
       expect(actualOutput).matchSnapshot();
     }
+
+    project.dispose();
   });
 });
