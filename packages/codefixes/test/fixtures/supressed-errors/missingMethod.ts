819d739e89c13f046e7e350c4ec7ca38526934d6
Matthew Edwards <medwards@linkedin.com>
2023-05-19 15:45:03 -0700

    chore: improve assertion and snapshot

--- packages/codefixes/test/fixtures/supressed-errors/missingMethod.ts
+++ packages/codefixes/test/fixtures/supressed-errors/missingMethod.ts
@@ -1,28 +1,28 @@
 import { Base } from 'some-package';
 
-class Something extends Base {
-  constructor(...args) {
-    super(...args);
+export class Something extends Base {
+  constructor() {
+    super();
     this.setRoutes({
-      'enterprise-cookies': '/mypreferences/e/enterprise-cookies/',
-      'feed.update': '/feed/update/:urn',
-      'help.ads': '/help/linkedin/answer/62931',
-      'help.binding-benefits': '/help/learning/answer/85873',
-      'help.certificates': '/help/learning/answer/82977',
-      'help.disability-answer-desk': '/help/linkedin/ask/DAD',
-      'help.instructor-analytics': '/help/learning/answer/128146',
-      'help.learning': '/help/learning',
-      'help.learning-privacy': '/help/learning/answer/71996',
-      'help.learning-progress': '/help/learning/answer/71903',
-      'help.learning-teams': '/help/learning/answer/a1376782',
-      'help.china': '/help/learning/answer/135087',
-      'help.likes': '/help/learning/answer/82032',
-      'help.lms-completion': '/help/learning/answer/129294',
-      'help.lynda-subscriber': '/help/lynda/answer/90509',
-      'help.questions-privacy': '/help/learning/answer/96677',
-      'help.reviews': '/help/learning/answer/121936',
-      'help.share-certificate': '/help/learning/answer/131504',
-      'help.sync-learning-history': '/help/learning/answer/120457',
+      'enterprise-cookies': '',
+      'feed.update': '',
+      'help.ads': '',
+      'help.binding-benefits': '',
+      'help.certificates': '',
+      'help.disability-answer-desk': '',
+      'help.instructor-analytics': '',
+      'help.learning': '',
+      'help.learning-privacy': '',
+      'help.learning-progress': '',
+      'help.learning-teams': '',
+      'help.china': '',
+      'help.likes': '',
+      'help.lms-completion': '',
+      'help.lynda-subscriber': '',
+      'help.questions-privacy': '',
+      'help.reviews': '',
+      'help.share-certificate': '',
+      'help.sync-learning-history': '',
     });
   }
 }
--- packages/codefixes/test/supressed-errors.test.ts
+++ packages/codefixes/test/supressed-errors.test.ts
@@ -11,11 +11,19 @@ const __dirname = dirname(__filename);
 
 describe('Supressed Errors', function () {
   const cases = [
-    [`TS2345: Cannot read properties of undefined (reading 'flags')`, 'supressed-errors/flags.ts'],
-    ['TS2339: False expression: Token end is child end', 'supressed-errors/missingMethod.ts'],
+    [
+      `TS2345: Cannot read properties of undefined (reading 'flags')`,
+      'supressed-errors/flags.ts',
+      `// @ts-expect-error @rehearsal TODO TS2345: Argument of type '{0}' is not assignable to parameter of type 'never'. Consider verifying both types, using type assertion: '(config.value as string)', or using type guard: 'if (config.value instanceof string) { ... }'.`,
+    ],
+    [
+      'TS2339: False expression: Token end is child end',
+      'supressed-errors/missingMethod.ts',
+      `// @ts-expect-error @rehearsal TODO TS2339: Property 'setRoutes' does not exist on type 'Something'.`,
+    ],
   ];
 
-  test.each(cases)('%s, %s', async (name, relativePathToFixture) => {
+  test.each(cases)('%s, %s', async (name, relativePathToFixture, expected) => {
     const fixtureDir = resolve(__dirname, 'fixtures');
     const project = Project.fromDir(fixtureDir, { linkDeps: true, linkDevDeps: true });
     await project.write();
@@ -41,7 +49,7 @@ describe('Supressed Errors', function () {
     }
 
     const actualOutput = readFileSync(file, 'utf-8');
-
+    expect(actualOutput).toContain(expected);
     expect(actualOutput).matchSnapshot();
 
     project.dispose();
