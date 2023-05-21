b05590f405ed56cd2541168e05d34fab2a929118
Matthew Edwards <medwards@linkedin.com>
2023-05-21 15:43:09 -0700

    chore: fix typos in names

--- packages/codefixes/test/suppressed-errors.test.ts
+++ packages/codefixes/test/suppressed-errors.test.ts
@@ -9,21 +9,22 @@ import { migrate } from '@rehearsal/migrate';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-describe('Supressed Errors', function () {
+describe('Suppressed Errors', function () {
   const cases = [
     [
       `TS2345: Cannot read properties of undefined (reading 'flags')`,
-      'supressed-errors/flags.ts',
+      'suppressed-errors/flags.ts',
       `// @ts-expect-error @rehearsal TODO TS2345: Argument of type '{0}' is not assignable to parameter of type 'never'. Consider verifying both types, using type assertion: '(config.value as string)', or using type guard: 'if (config.value instanceof string) { ... }'.`,
     ],
     [
       'TS2339: False expression: Token end is child end',
-      'supressed-errors/missingMethod.ts',
+      'suppressed-errors/missingMethod.ts',
       `// @ts-expect-error @rehearsal TODO TS2339: Property 'setRoutes' does not exist on type 'Something'.`,
     ],
   ];
 
-  test.each(cases)('%s, %s', async (name, relativePathToFixture, expected) => {
+  test.each(cases)('%s, %s', async (...args) => {
+    const [, relativePathToFixture, expected] = args;
     const fixtureDir = resolve(__dirname, 'fixtures');
     const project = Project.fromDir(fixtureDir, { linkDeps: true, linkDevDeps: true });
     await project.write();
