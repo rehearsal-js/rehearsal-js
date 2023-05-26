c0b1ffcd447aef97a7f2bf90c3c288f5cfaffa4c
Matt Edwards <medwards@linkedin.com>
2023-05-25 17:16:39 -0700

    fix(plugins): service string resolution to support package with org in transform plugin  (#1091)

--- packages/migrate/test/fixtures/project/src/with-addon-service.ts
+++ packages/migrate/test/fixtures/project/src/with-addon-service.ts
@@ -3,4 +3,6 @@ import { inject as service } from "@ember/service";
 
 export default class SomeTsComponent extends Component {
   @service("my-addon@foo") foo;
+
+  @service("@my-org/some-ember-addon@LocaleService") locale;
 }
--- packages/migrate/test/fixtures/project/src/with-mapped-service.ts
+++ packages/migrate/test/fixtures/project/src/with-mapped-service.ts
@@ -4,6 +4,8 @@ import { inject as service } from "@ember/service";
 export default class SomeTsComponent extends Component {
   @service("boo-service") booService;
 
+  @service("@some-org/some-package@mapped") something;
+
   @service gooService;
 
   @service mooService;
