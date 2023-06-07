3bdebc1c973b5d7798b6414085103a1aab9bdfce
Matthew Edwards <medwards@linkedin.com>
2023-06-06 23:24:46 -0700

    bug: repro for boundary issue #1110

--- packages/smoke-test/test/commands/fix.test.ts
+++ packages/smoke-test/test/commands/fix.test.ts
@@ -81,4 +81,92 @@ describe('fix command', () => {
     });
     expect(cleanOutput(result.stdout, project.baseDir)).toMatchSnapshot();
   });
+
+  test.todo('should not fail if file reference out of package bondary', async () => {
+    project = prepareProject('base_ts_app');
+
+    project.mergeFiles({
+      'module-a': {
+        'tsconfig.json': `
+        {
+          "include": ['src/**/*.ts'],
+          "compilerOptions": {
+            "paths": {
+              "module-b/*": ["../module-b/dist/src/*"]
+            }
+          }
+        }
+        `,
+        src: {
+          'checker.js': `
+            import { IS_BROWSER } from "module-b/is-browser";
+            console.log(IS_BROWSER);
+          `,
+        },
+      },
+      'module-b': {
+        'package.json': `
+        {
+          "name": "module-b",
+          "version": "1.0.0",
+          "main": "index.js",
+          "license": "MIT",
+          "dependencies": {
+            "path": "*"
+          },
+          "exports": {
+            ".": {
+              "types": "./dist/src/index.d.ts",
+              "import": "./dist/src/index.js"
+            },
+            "./*": "./dist/src/*"
+          },
+          "main": "dist/src/index.js",
+          "types": "dist/src/index.d.ts",
+          "files": [
+            "dist"
+          ],
+          "packageManager": "pnpm@7.12.1"
+        }
+        `,
+        'tsconfig.json': `{ include: ['src']}`,
+        src: {
+          'is-browser.ts': `
+          const IS_BROWSER = true;
+          export default IS_BROWSER;
+          `,
+        },
+        dist: {
+          src: {
+            'is-browser.d.ts': '',
+            'is-browser.js': `
+              const IS_BROWSER = true;
+              export default IS_BROWSER;
+            `,
+          },
+        },
+      },
+    });
+    //
+    await project.write();
+
+    // move to .ts
+    await rehearsalCLI('move', './module-a', ['--rootPath', project.baseDir], {
+      cwd: project.baseDir,
+    });
+
+    const result = await rehearsalCLI(
+      'fix',
+      './module-a',
+      ['--graph', '--rootPath', project.baseDir, '--ignore', 'vitest.config.ts,test/**'],
+      {
+        cwd: project.baseDir,
+      }
+    );
+
+    expect(cleanOutput(result.stderr, project.baseDir)).not.contains(
+      '[FAILED] Could not find source file: <tmp-path>/module-b/dist/src/is-browser.js'
+    );
+    expect(false).toBe(true);
+  });
 });
