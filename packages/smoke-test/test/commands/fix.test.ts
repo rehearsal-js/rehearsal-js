052ee7ec680027ffc7a3a806a93832a303625f2b
Matthew Edwards <medwards@linkedin.com>
2023-07-20 10:06:48 -0700

    chore(smoke-test): add coverage for glint codefixes
    - Moved glint codefix tests to smoke-test
    - Add prettier config for .gjs/.gts files for formatting output

--- packages/smoke-test/test/commands/fix.test.ts
+++ packages/smoke-test/test/commands/fix.test.ts
@@ -69,6 +69,29 @@ describe('fix command', () => {
     expectFile(join(projectRoot, 'app/components/hello-world.gts')).toMatchSnapshot();
   });
 
+  test('ember_js_app_4.11 --mode=drain', async () => {
+    project = prepareProject('ember_js_app');
+    await project.write();
+    const projectRoot = resolve(project.baseDir);
+
+    // move to .ts
+    await rehearsalCLI('move', './app', ['--rootPath', project.baseDir], {
+      cwd: project.baseDir,
+    });
+    // infer types and TODOs
+    const result = await rehearsalCLI(
+      'fix',
+      './app',
+      ['--rootPath', project.baseDir, '--mode', 'drain'], // Invokes fix with drain.
+      {
+        cwd: project.baseDir,
+      }
+    );
+
+    expect(cleanOutput(result.stdout, project.baseDir)).toMatchSnapshot();
+    expectFile(join(projectRoot, 'app/components/hello-world.gts')).toMatchSnapshot();
+  });
+
   test('glimmerx_js_app', async () => {
     project = prepareProject('glimmerx_js_app');
     const projectRoot = resolve(project.baseDir);
--- packages/smoke-test/test/fixtures/ember_js_app/.prettierrc.js
+++ packages/smoke-test/test/fixtures/ember_js_app/.prettierrc.js
@@ -8,5 +8,19 @@ module.exports = {
         singleQuote: true,
       },
     },
+    {
+      files: '*.hbs',
+      options: {
+        parser: 'glimmer',
+      },
+    },
+    {
+      files: '*.gjs, *.gts',
+      options: {
+        parser: 'glimmer',
+        singleQuote: true,
+      },
+    },
   ],
+  plugins: ['prettier-plugin-ember-template-tag'],
 };
